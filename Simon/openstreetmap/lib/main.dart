d160f2f67985c0e4acbdb25dc26f281f87f31a8d
Simon <sim.coatanlem.35@gmail.com>
2025-03-17 08:42:43 +0100

    FIX: Clean Repo

--- Simon/openstreetmap/lib/main.dart
+++ Simon/openstreetmap/lib/main.dart
@@ -1,81 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:latlong2/spline.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-
-   Widget content() {
-    return FlutterMap(
-      options: MapOptions(
-        initialCenter: LatLng(48.733333, -3.449757),
-        initialZoom: 30,
-        interactionOptions: 
-          const InteractionOptions(flags: ~InteractiveFlag.doubleTapDragZoom),
-      ),
-      children: [
-        openStreetMapTileLayer,
-      ],
-    );
-  }
-}
-
-TileLayer get openStreetMapTileLayer => TileLayer(
-  urlTemplate: 'https://tile.openstreetmap.org/#18/48.731745/-3.450657.png',
-  userAgentPackageName: 'dev.fleaflet.flutter_map.exemple',
-);
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class UmapFlutterMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Open Street Map',
-          style: TextStyle(fontSize: 22),
+      appBar: AppBar(title: Text("Carte uMap")),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(48.8566, 2.3522), // Coordonnées de Paris (remplace par celles de ta carte)
+          initialZoom: 10.0,
         ),
+        children: [
+          TileLayer(
+            urlTemplate: "https://umap.openstreetmap.fr/fr/map/testosm_1189050",
+            subdomains: ['a', 'b', 'c'],
+          ),
+        ],
       ),
-      body: content(),
     );
   }
-
-  Widget content() {
-    return FlutterMap(
-      options: MapOptions(
-        initialCenter: LatLng(48.733333, -3.449757),
-        initialZoom: 11,
-        interactionOptions: 
-          const InteractionOptions(flags: ~InteractiveFlag.doubleTapDragZoom),
-      ),
-      children: [
-        openStreetMapTileLayer,
-      ],
-    );
-  }
-
-  TileLayer get openStreetMapTileLayer => TileLayer(
-    urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-    userAgentPackageName: 'dev.fleaflet.flutter_map.exemple',
-  );
 }
-
